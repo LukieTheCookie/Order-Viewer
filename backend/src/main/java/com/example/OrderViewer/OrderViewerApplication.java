25ad1400a542b8c0d9f61ed1ac5caa3048258fdc
Luqmaan Kamaar <l.kamaar10@gmail.com>
2025-07-01 20:19:15 +0200

    Initial commit. Added Angular project and Spring + DB connection

---
+++ backend/src/main/java/com/example/OrderViewer/OrderViewerApplication.java
@@ -0,0 +1,13 @@
+package com.example.OrderViewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderViewerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderViewerApplication.class, args);
+	}
+
+}
